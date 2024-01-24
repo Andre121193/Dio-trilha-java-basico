71394cf71dc058e4e9b2bf024ace8501d94da0d5
Andre <andre1993andre@hotmail.com>
2024-01-23 21:23:44 -0300

    traçando os objetivos do projeto

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+       // TODO: Conhecer e importar a classe scanner
+
+        //TODO: Exibir as mensagens ao usuario
+
+        // TODO: obter os valores utilizando o scanner
+
+        // TODO: exibir a mensagem formatada
+
+    }
+}
